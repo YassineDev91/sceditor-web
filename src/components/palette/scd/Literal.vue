86066d9be0b714ff5cc51f3c68bfc4b432ca42d5
YassineDev91 <yassine.dev@outlook.com>
2026-07-29 06:32:21 +0100

    feat(canvas): render TypeNode types on Variable and Struct-field nodes

--- src/components/palette/scd/Literal.vue
+++ src/components/palette/scd/Literal.vue
@@ -3,14 +3,17 @@
         <v-rect ref="textRectRef" :config="textRectConfig"></v-rect>
         <!-- <v-text ref="textRectRef" :config="visibilityConfig"></v-text> -->
         <v-text ref="literalTextRef" :config="nameConfig"></v-text>
-        <!-- <v-text :config="commaConfig"></v-text>
-        <v-text :config="typeConfig"></v-text> -->
+        <v-text v-if="props.data?.type" :config="typeConfig"></v-text>
     </v-group>
 </template>
 
 <script setup>
 import { onMounted, ref } from "vue";
 import { computed } from 'vue'
+import { formatTypeNode } from '@/schema/typeFormat'
+import { useContractStorage } from '@/stores/contract'
+
+const fileStore = useContractStorage()
 
 var textRectRef = ref({})
 var literalTextRef = ref({})
@@ -45,19 +48,20 @@ const nameConfig = computed(() =>({
     text: props.name,
     fontSize: 12,
 }))
-// const typeConfig = ref({
-//     x: nameConfig.value.x + nameConfig.value.fontSize*5,
-//     y: nameConfig.value.y,
-//     text: props.type,
-//     fontSize: 12,
-// })
-
-// const commaConfig = ref({
-//     x: nameConfig.value.x + nameConfig.value.fontSize*4,
-//     y: nameConfig.value.y,
-//     text: ":",
-//     fontSize: 12,
-// })
+const typeConfig = computed(() => ({
+    x: nameConfig.value.x + 55,
+    y: nameConfig.value.y,
+    text: props.data?.type
+        ? formatTypeNode(props.data.type, {
+            resolveRef: (id) =>
+              fileStore.contract.structs.find(s => s.id === id)?.name ||
+              fileStore.contract.enums.find(e => e.id === id)?.name ||
+              id,
+          })
+        : '',
+    fontSize: 11,
+    fill: '#7C6BAE',
+}))
 onMounted(() => {
   
 
